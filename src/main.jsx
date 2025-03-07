0b028693a329450b6ea2945e015d7c49b73a8417
XatoPo <flaviovm2013@gmail.com>
2025-03-07 09:25:39 -0500

    `Added @vercel/speed-insights dependency and its usage in main.jsx`

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';  // Asegúrate de importar el componente
+import './index.css';
+import App from './App.jsx';
 
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights />  {/* Agregar este componente aquí */}
     <App />
-  </StrictMode>,
-)
-
+  </StrictMode>
+);
